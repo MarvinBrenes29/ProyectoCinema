1391b918d6556989505d35f7f504c11ef7d04b77
Marvin Brenes <marvin9629@gmail.com>
2022-04-06 21:41:02 -0600

    Login - Registrarse agregados

--- js/app.js
+++ js/app.js
@@ -129,6 +129,13 @@ if(er.test(email.value) !== '' && nombre.value !== '' && mensaje.value !== ''){
 
 
 
- //Swiper
+ //Mapa
+ var map = L.map('map').setView([51.505, -0.09], 13);
 
+ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+ }).addTo(map);
  
+ L.marker([51.5, -0.09]).addTo(map)
+     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+     .openPopup();
