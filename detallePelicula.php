8dbc4810e1ad8a0de6725f57408970a4c9fd251a
alsolis123 <cordoba.solis16@gmail.com>
2022-04-08 22:08:25 -0600

    agregue shared y footer

---
+++ detallePelicula.php
@@ -0,0 +1,107 @@
+<?php
+include "shared/header.php";
+?>
+  <section class="hero">
+    <div>
+      <iframe width="500" height="300" src="https://www.youtube.com/embed/u9Mv98Gr5pY" title="YouTube video player"
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+    </div>
+
+  </section>
+
+  <section class="search">
+    <h2 class="search__heading">Lorem Ipsum </h2>
+    <div class="container">
+      <form class="form">
+        <select class="form__seccion">
+          <option value="">-</option>
+          <option>San José</option>
+          <option>Alajuela</option>
+          <option>Heredia</option>
+          <option>Cartago</option>
+
+        </select>
+        <select class="form__seccion">
+          <option>-</option>
+          <option>1:45PM</option>
+          <option>5:00PM</option>
+          <option>8:00PM</option>
+          <option>Marketing</option>
+        </select>
+        <select class="form__seccion">
+          <option>-</option>
+          <option>VIP</option>
+          <option>Regular</option>
+          <option>Adulto Mayor</option>
+          <option>Niños</option>
+
+        </select>
+        <input type="submit" class="form__submit" value="Continuar">
+      </form>
+    </div>
+  </section>
+
+  <main class="movie__info container ">
+    <div class="movie__grid">
+      <div class="w-50">
+        <h1 class="movie__heading">Lorem Ipsum</h1>
+        <p class="movie__resume">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam hic praesentium vitae
+          inventore animi ipsam dolorem provident</p>
+
+        <div>
+          <div class="movie__description">
+            <h4 class="movie__titles">Director</h6>
+              <p class="movie">Chris Milewski</p>
+          </div>
+
+          <div class="movie__description">
+            <h4 class="movie__titles">Casting</h6>
+              <p>Andrea Autulio, Jeffery Voice, Chiara Pavoni</p>
+          </div>
+
+          <div class="movie__description">
+            <h4 class="movie__titles">Producción</h6>
+              <p>Digiflex</p>
+          </div>
+
+        </div>
+
+
+      </div>
+
+      <div>
+        <img class="movie__img" src="img/dog.jpg" alt="">
+      </div>
+    </div>
+  </main>
+
+
+  <footer class="footer">
+    <div class="footer__contenido">
+      <div>
+        <a href="index.html">
+          <img class="footer__logo" src="img/Logo.png" alt="">
+        </a>
+      </div>
+      <div class="footer__redes">
+        <a href="">
+          <img class="footer__social" src="img/facebook.png" alt="facebook">
+        </a>
+        <a href="#">
+          <img class="footer__social" src="img/instagram.png" alt="instagram">
+        </a>
+        <a href="#">
+          <img class="footer__social" src="img/youtube .png" alt="youtube">
+        </a>
+      </div>
+
+      <div>
+        <p class="footer__texto">Todos los derechos reservados &copy</p>
+      </div>
+
+
+    </div>
+<?php
+include "shared/footer.php";
+?>
--- index.php
+++ index.php
@@ -1,67 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,100&display=swap"
-    rel="stylesheet">
-  <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/styles.css">
-  <title>Cinema</title>
-</head>
-
-<body>
-
-  <!-- Navbar  -->
-  <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top mt-10 ">
-    <div class="container">
-      <a href="index.html">
-        <img class="logo" src="img/Logo.png" alt="">
-      </a>
-
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
-        <span class="navbar-toggler-icon "></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navmenu">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a href="index.html" class="nav-link">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a href="pelicula.html" class="nav-link">Películas</a>
-          </li>
-          <li class="nav-item">
-            <a href="nosotros.html" class="nav-link">Nosotros</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false">
-              Administración
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Administración de banners</a>
-              <a class="dropdown-item" href="#">Administración de Salas</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Administración de Películas</a>
-              <a class="dropdown-item" href="#">Administración de Horarios</a>
-            </div>
-          </li>
-
-          <li class="nav-item">
-            <a href="login.html" class="nav-link--button">Iniciar Sesión</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+<?php
+include "shared/header.php";
+?>
 
 
   <!--.slider-->
@@ -115,7 +54,7 @@
     <div class="pelicula">
       <div class="swiper-container swiper">
         <div class="swiper-wrapper">
-          <div class="swiper-slide"><img class="img__pelicula" src="img/dog.jpg" alt="Movie Title"> Some Text</div>
+          <div type="submit" class="swiper-slide"><img class="img__pelicula" src="img/dog.jpg" alt="Movie Title"> Some Text</div>
           <div class="swiper-slide"><img class="img__pelicula" src="img/dog.jpg" alt="Movie Title">Some Text</div>
           <div class="swiper-slide"><img class="img__pelicula" src="img/dog.jpg" alt="Movie Title">Some Text</div>
           <div class="swiper-slide"><img class="img__pelicula" src="img/dog.jpg" alt="Movie Title">Some Text</div>
@@ -157,13 +96,7 @@
 
 
     </div>
-    <!--.footer__contenido-->
-  </footer>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-    crossorigin="anonymous"></script>
-  <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-  <script src="js/apps.js"></script>
-</body>
-
-</html>
+<?php
+include "shared/footer.php";
+?>
+  
