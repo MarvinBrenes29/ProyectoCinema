2bd7612e1a389c3a80917e40c276fcb8accb30a5
alsolis123 <cordoba.solis16@gmail.com>
2022-04-14 01:41:05 -0600

    CambiosdeMiercolesNoche
    termine CRUD de peliculas y salas

--- model/clsAdm_peliculas.php
+++ model/clsAdm_peliculas.php
@@ -16,6 +16,9 @@ class clsAdm_peliculas extends clsConexion{
      public $sinopsis;
      public $trailer;
      public $estado;
+     public $nombreSala;
+     public $tipo_sala;
+     public $estado_sala;
 
      public function __construct()
      {
@@ -36,6 +39,7 @@ class clsAdm_peliculas extends clsConexion{
          $dato->sinopsis = $sinopsis;
          $dato->trailer = $trailer;
          $dato->estado = $estado;
+
          return $dato;
 
      }
@@ -187,9 +191,278 @@ class clsAdm_peliculas extends clsConexion{
     }
 }
 
+public function crearFilasPeliculas(){
+
+        $dbh = $this->conectar();
+        if ($dbh != null) {
+            $consulta = $dbh->prepare("SELECT idpelicula, titulo, actor_principal, actor_secundario, director, clasificacion, duracion_minutos, genero, idioma, sinopsis, trailer, estado  FROM adm_pelicula");
+            //determinar el motos de fetch
+            $consulta->setFetchMode(PDO::FETCH_ASSOC); //resultado como un arreglo asociativo
+            $consulta->execute();
+            $filas = "";
+
+            foreach ($consulta as $key => $v) {
+                // .= es para concatenar
+                $filas .= "<tr>
+                                <td scope='col' ><img class='crudPeli' src='img/peliculas/$v[idpelicula].jpg' alt=''></td>
+                                <td scope='col'>$v[idpelicula]</td>
+                                <td scope='col'>$v[titulo]</td>
+                                <td scope='col'>$v[actor_principal]</td>
+                                <td scope='col'>$v[actor_secundario]</td>
+                                <td scope='col'>$v[director]</td>
+                                <td scope='col'>$v[clasificacion]</td>
+                                <td scope='col'>$v[duracion_minutos]</td>
+                                <td scope='col'>$v[genero]</td>
+                                <td scope='col'>$v[idioma]</td>
+                                <td scope='col'>$v[sinopsis]</td>
+                                <td scope='col'>$v[trailer]</td>
+                                <td scope='col'>$v[estado]</td>
+                                <td scope='col'>
+                                    <a href='modificarPelicula.php?idpelicula=$v[idpelicula]' class='btn btn-warning'><i class='fas fa-pencil-alt'></i></a>
+                                    <a href='eliminarPelicula.php?idpelicula=$v[idpelicula]' class='btn btn-danger'><i class='fas fa-trash-alt'></i></a>
+                                </td>
+                            </tr>";
+            }
+            $dbh=null;
+            return $filas;
+        }
+    }
+
+    public function consultarPelicula($idpelicula){
+        $dbh = $this->conectar();
+        if ($dbh != null) {
+            $consulta = $dbh->prepare("SELECT idpelicula, titulo, actor_principal, actor_secundario, director, clasificacion, duracion_minutos, genero, idioma, sinopsis, trailer, estado  FROM adm_pelicula where idpelicula=$idpelicula");
+
+            
+            $consulta->execute();
+            $res=$consulta->fetchAll(PDO::FETCH_OBJ);
+            $dbh=null;
+            
+            return $res[0];
+        }
+    }
+
+    public function eliminar_pelicula(){
+
+        $dbh = $this->conectar();
+        if ($dbh != null) {
+            try {
+                
+
+                $consulta = $dbh->prepare("DELETE FROM adm_pelicula 
+                                            WHERE idpelicula = :idpelicula");
+
+               
+                $consulta->bindParam(":idpelicula", $this->idpelicula);
+
+                
+                $consulta->execute();
+                $estado = true;
+                if(file_exists('img/peliculas/'.$this->idpelicula.'.jpg')){
+                    unlink('img/peliculas/'.$this->idpelicula.'.jpg');
+                }
+            } catch (PDOException $e) {
+                $estado = false;
+                $e->getMessage();
+                echo $e;
+            } finally {
+                //cerrar la conexion a la bd
+                $dbh = null;
+                return $estado;
+            }
+        }
+    }
+
+    public function modificar_pelicula(){
+
+        $dbh = $this->conectar();
+        if ($dbh != null) {
+            try {
+                
+
+                $consulta = $dbh->prepare("UPDATE adm_pelicula SET titulo=:titulo, actor_principal=:actor_principal, actor_secundario=:actor_secundario, director=:director, clasificacion=:clasificacion, duracion_minutos=:duracion_minutos, genero=:genero, idioma=:idioma, sinopsis=:sinopsis, trailer=:trailer, estado=:estado WHERE idpelicula=:idpelicula");
+
+               
+                
+                $consulta->bindParam(":titulo", $this->titulo);
+                $consulta->bindParam(":actor_principal", $this->actor_principal);
+                $consulta->bindParam(":actor_secundario", $this->actor_secundario);
+                $consulta->bindParam(":director", $this->director);
+                $consulta->bindParam(":clasificacion", $this->clasificacion);
+                $consulta->bindParam(":duracion_minutos", $this->duracion);
+                $consulta->bindParam(":genero", $this->genero);
+                $consulta->bindParam(":idioma", $this->idioma);
+                $consulta->bindParam(":sinopsis", $this->sinopsis);
+                $consulta->bindParam(":trailer", $this->trailer);
+                $consulta->bindParam(":estado", $this->estado);
+                $consulta->bindParam(":idpelicula", $this->idpelicula);
+
+                
+                $consulta->execute();
+                $estado = true;
+            } catch (PDOException $e) {
+                $estado = false;
+                $e->getMessage();
+            } finally {
+                $dbh = null;
+                return $estado;
+            }
+        }
+    }
+// ----------------------------------------------------------------------------------SALAS
+    public function verificar_siexite_sala($nombreSala_parametro){
+        $dbh = $this->conectar();
+        if ($dbh != null) {
+            $condicion = false;
+            $consulta = $dbh->prepare("SELECT nombreSala FROM adm_sala where nombreSala = :nombreSala");
+            
+            $consulta->bindParam(":nombreSala", $nombreSala_parametro);
+
+            // Conseguir arreglo asociativo
+            $consulta->setFetchMode(PDO::FETCH_ASSOC);
+            $consulta->execute();
+
+            $contador = $consulta -> rowCount();
+
+            if($contador==1){
+                $condicion = true;
+            }
+            
+            }
+            $dbh=null;
+            
+            
+            return $condicion;
+        }
 
 
+    public function insertar_sala($nombreSala_parametro, $tipo_sala_parametro, $estado_parametro) {
 
+        $dbh = $this->conectar();
+        if ($dbh != null) {
+            try {
+                
+                $consulta_login = $dbh->prepare("INSERT INTO adm_sala (nombreSala, tipo_sala, estado) VALUES(:nombreSala, :tipo_sala, :estado)");
+
+                
+                $consulta_login->bindParam(":nombreSala", $nombreSala_parametro);
+                $consulta_login->bindParam(":tipo_sala", $tipo_sala_parametro);
+                $consulta_login->bindParam(":estado", $estado_parametro);
+                
+
+
+                $consulta_login->execute();
+                $estado = true;
+                echo "se ingresaron los datos";
+            } catch (PDOException $e) {
+                $estado = false;
+                $e->getMessage();
+                echo 'Lo sentimos, nos encontramos teniendo inconvenientes';
+            } finally {
+                $dbh = null;
+                return $estado;
+            }
+        }
+    }
+
+
+    public function crearFilasSalas(){
+
+        $dbh = $this->conectar();
+        if ($dbh != null) {
+            $consulta = $dbh->prepare("SELECT nombreSala, tipo_sala, estado FROM adm_sala");
+
+            $consulta->setFetchMode(PDO::FETCH_ASSOC); 
+            $consulta->execute();
+            $filas = "";
+
+            foreach ($consulta as $key => $v) {
+                $filas .= "<tr>
+                                
+                                <td scope='col'>$v[nombreSala]</td>
+                                <td scope='col'>$v[tipo_sala]</td>
+                                <td scope='col'>$v[estado]</td>
+                                <td scope='col'>
+                                    <a href='modificarSala.php?nombreSala=$v[nombreSala]' class='btn btn-warning'><i class='fas fa-pencil-alt'></i></a>
+                                    <a href='eliminarSala.php?nombreSala=$v[nombreSala]' class='btn btn-danger'><i class='fas fa-trash-alt'></i></a>
+                                </td>
+                            </tr>";
+            }
+            $dbh=null;
+            return $filas;
+        }
+    }
+
+    public function modificar_sala(){
+
+        $dbh = $this->conectar();
+        if ($dbh != null) {
+            try {
+                
+
+                $consulta = $dbh->prepare("UPDATE adm_sala SET nombreSala=:nombreSala, tipo_sala=:tipo_sala, estado=:estado WHERE nombreSala=:nombreSala");
+
+                
+                $consulta->bindParam(":nombreSala", $this->nombreSala);
+                $consulta->bindParam(":tipo_sala", $this->tipo_sala);
+                $consulta->bindParam(":estado", $this->estado_sala);
+
+                
+                $consulta->execute();
+                $estado = true;
+            } catch (PDOException $e) {
+                $estado = false;
+                $e->getMessage();
+            } finally {
+                $dbh = null;
+                return $estado;
+            }
+        }
+    }
+
+    public function consultarSala($nombreSala_parametro){
+        $dbh = $this->conectar();
+        if ($dbh != null) {
+            $consulta = $dbh->prepare("SELECT nombreSala, tipo_sala, estado FROM adm_sala where nombreSala=$nombreSala_parametro");
+
+            
+            $consulta->execute();
+            $res=$consulta->fetchAll(PDO::FETCH_OBJ);
+            
+            $dbh=null;
+            
+            return $res[0];
+        }
+    }
+
+    public function eliminar_sala($nombreSala){
+
+        $dbh = $this->conectar();
+        if ($dbh != null) {
+            try {
+                
+
+                $consulta = $dbh->prepare("DELETE FROM adm_sala
+                                            WHERE nombreSala = :nombreSala");
+
+               
+                $consulta->bindParam(":nombreSala", $nombreSala);
+
+                
+                $consulta->execute();
+                $estado = true;
+                
+            } catch (PDOException $e) {
+                $estado = false;
+                $e->getMessage();
+                echo $e;
+            } finally {
+                //cerrar la conexion a la bd
+                $dbh = null;
+                return $estado;
+            }
+        }
+    }
 
 
 }
